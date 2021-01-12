1bb2b08962e1a66377ccdd2b5b12aeb7d2b5341d
TING2938 <yeting2938@hust.edu.cn>
2021-01-12 10:27:13 +0800

    push

--- build_2020.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-## usage
-# source path/to/GMXRC
-# ./build_2020.sh path/to/your/code
-
-basename=${1%%.c*}
-g++ $* -o $basename -I $GMXPREFIX/include -I ./include -L $GMXLDLIB -lgromacs -O3 -std=c++17
-
