2d2a95a96296ad93dc4008c18a3bf1e994079dcc
TING2938 <yeting2938@hust.edu.cn>
2021-05-28 18:41:01 +0800

    push

---
+++ src/test/testBinaryIOSpeed-TING.cpp
@@ -0,0 +1,61 @@
+#include <vector>
+#include <iomanip>
+#include <itp/core>
+#include <fstream>
+#include <itp/timer>
+#include <sstream>
+#include <iostream>
+
+using real = float;
+
+int main()
+{
+	int N;
+	fmt::print("please input matrix size: ");
+	std::cin >> N;
+	fmt::print("Matrix size: {0} x {0}\n", N);
+	std::vector<std::vector<real>> matrix(N);
+	for (int i = 0; i < N; i++)
+	{
+		matrix[i].resize(N);
+		for (int j = 0; j < N; j++)
+		{
+			matrix[i][j] = i * i * 1.32 + j * 4.23 + 1.42;
+		}
+	}
+
+	itp::Timer timer;
+
+
+	std::ofstream binFile("binFile", std::ios::binary);
+	timer.start();
+	for (int i = 0; i < N; i++)
+	{
+		binFile.write((char*)(matrix[i].data()), sizeof(real) * N);
+	}
+	timer.stop();
+	binFile.close();
+	fmt::print("write to binary file: {} s\n", timer.span());
+
+	std::vector<real> loadFromBinary(N*N);
+	std::ifstream binFileID("binFile", std::ios::binary);
+	timer.start();
+	binFileID.read((char*)(loadFromBinary.data()), sizeof(real)*N*N);
+	timer.stop();
+	binFileID.close();
+	fmt::print("read from binary file: {} s\n", timer.span());
+	
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (loadFromBinary[i * N + j] != matrix[i][j])
+			{
+				fmt::print("Not equal: [{}][{}]: {} and {}\n", i, j, loadFromBinary[i * N + j], matrix[i][j]);
+				std::exit(-1);
+			}
+		}
+	}
+	fmt::print("All equal\n");
+}
+
--- src/test/tst4.cpp
+++ src/test/tst4.cpp
@@ -1,7 +1,17 @@
 #include <itp/core>
 
+#include <format>
+
+#define Print(...) std::cout << std::format(__VA_ARGS__)
+
 int main()
 {
 	Eigen::Vector3d vec(1, 3, 5);
 	std::cout << vec << std::endl;
+
+	std::cout << std::format("a is {}\n", 42);
+
+	Print("b is {} {}\n", 43, 42);
+	
 }
+
